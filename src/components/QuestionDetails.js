22fad444d49dac3047b3e4164fd7c99f4f828c7e
helgakern <helgakern@gmail.com>
2021-06-16 11:47:50 -0700

    Make QuestionIndexPage and QuestionShowPage a functional component to use useState and useEffect hooks

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -1,28 +1,52 @@
 import React from 'react';
 
-// A React component is a function that returns a React element,
-// which are created with React.createElement() or JSX tags.
-// Our components' names must be in PascalCase, otherwise, it will
-// look for an plain HTML tag when we use it.
-function QuestionDetails({ title, body, author, view_count, created_at }){
-    return(
-      <div>
-        <h2>{title}</h2>
+export const QuestionDetails = props => {
+  console.log(props)
+  return (
+    <div>
+        <h2>{props.title}</h2>
         <p
         style={{
           fontStyle: 'Roboto',
           fontSize: '24px'
-        }}
-        >{body}</p>
-        <p>By: {author? author.full_name : null}</p>
+          }}
+        >{props.body}</p>
+        <p>By {props.author && props.author.full_name}</p>
         <div>
-          <small>View Count: {view_count}</small>
+          <small>View Count: {props.view_count}</small>
           <small
-          style={{ marginLeft: '20px'}}
-          >Asked {created_at}</small>
+          style={{ marginLeft: '20px' }}
+          >Asked {props.created_at}</small>
         </div>
       </div>
-    )
-  }
+  );
+ };
+ 
 
-  export default QuestionDetails;
+
+// A React component is a function that returns a React element,
+// which are created with React.createElement() or JSX tags.
+// Our components' names must be in PascalCase, otherwise, it will
+// look for an plain HTML tag when we use it.
+// function QuestionDetails({ title, body, author, view_count, created_at }){
+//     return(
+//       <div>
+//         <h2>{title}</h2>
+//         <p
+//         style={{
+//           fontStyle: 'Roboto',
+//           fontSize: '24px'
+//         }}
+//         >{body}</p>
+//         <p>By: {author? author.full_name : null}</p>
+//         <div>
+//           <small>View Count: {view_count}</small>
+//           <small
+//           style={{ marginLeft: '20px'}}
+//           >Asked {created_at}</small>
+//         </div>
+//       </div>
+//     )
+//   }
+
+//   export default QuestionDetails;
--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,8 +1,51 @@
-import React, {Component} from 'react';
-import QuestionDetails from './QuestionDetails';
+import React, {useState, useEffect} from 'react';
+import {QuestionDetails} from './QuestionDetails';
 import AnswerList from './AnswerList';
 // import questionData from '../data/questionData'
 import {Question} from '../requests'
+
+// Move QuestionShowPage component into a function component, and make sure to use both hooks,
+// useState and useEffect if necessary.
+
+const QuestionShowPage = props => {
+  const [questionShow, setQuestionShow] = useState();
+  
+  const deleteQuestion = () => {
+    Question.destroy(questionShow.question.id).then(data => {
+      props.history.push("/questions");
+    });
+  };
+  
+  const deleteAnswer = id => {
+    const newAnswers = questionShow.question.answers.filter(a => a.id !== id);
+    setQuestionShow({
+      ...questionShow,
+      question: { ...questionShow.question, answers: newAnswers }
+    });
+  };
+  
+  useEffect(() => {
+    Question.show(props.match.params.id).then(question => {
+      setQuestionShow(question);
+    });
+  }, []);
+  
+  return (
+    <main>
+        <QuestionDetails {...questionShow} />
+        <button onClick={() => deleteQuestion()}>Delete</button>
+        {questionShow && questionShow.id && questionShow.answers?.length> 0 ?
+        <AnswerList
+        answers={questionShow?.answers}
+        onAnswerDeleteClick={id => deleteAnswer(id)}
+        /> : " "
+        }
+      </main>
+  );
+ };
+  
+ export default QuestionShowPage; 
+
 
 
 // To compose our application, we will create components that nest other components.
@@ -11,51 +54,51 @@ import {Question} from '../requests'
 // However, you can nest as many components inside as you want, just like how you can nest
 // as many objects as you want inside of the object you return.
 
-class QuestionShowPage extends Component {
-  constructor(props){
-    super(props);
-    this.state = { question : {} }
-    this.deleteAnswer = this.deleteAnswer.bind(this)
-  }
+// class QuestionShowPage extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state = { question : {} }
+//     this.deleteAnswer = this.deleteAnswer.bind(this)
+//   }
 
-  componentDidMount(){
-    Question.show(this.props.match.params.id)
-    .then((question) => {
-      this.setState((state) => {
-        return {
-          question: question
-        }
-      })
-    })
-  }
+//   componentDidMount(){
+//     Question.show(this.props.match.params.id)
+//     .then((question) => {
+//       this.setState((state) => {
+//         return {
+//           question: question
+//         }
+//       })
+//     })
+//   }
 
-  deleteAnswer(id){
-    this.setState((state) => {
-      return {
-        answers: state.answers.filter(a => a.id !== id)
-      }
-    })
-  }
+//   deleteAnswer(id){
+//     this.setState((state) => {
+//       return {
+//         answers: state.answers.filter(a => a.id !== id)
+//       }
+//     })
+//   }
 
 
-  render(){
-    const { title, body, author, view_count, created_at } = this.state.question
-    return(
-      <main>
-        <QuestionDetails 
-        title={title}
-        body={body}
-        author={author}
-        view_count={view_count}
-        created_at={created_at}
-        />
-        <AnswerList
-        answers={this.state.answers}
-        deleteAnswer={this.deleteAnswer}
-        />
-      </main>
-    )
-  }
-}
+//   render(){
+//     const { title, body, author, view_count, created_at } = this.state.question
+//     return(
+//       <main>
+//         <QuestionDetails 
+//         title={title}
+//         body={body}
+//         author={author}
+//         view_count={view_count}
+//         created_at={created_at}
+//         />
+//         <AnswerList
+//         answers={this.state.answers}
+//         deleteAnswer={this.deleteAnswer}
+//         />
+//       </main>
+//     )
+//   }
+// }
 
-export default QuestionShowPage;
+// export default QuestionShowPage;
